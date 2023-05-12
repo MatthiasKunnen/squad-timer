3383f9866bdd182939443d9de8ae206fde9294eb
Matthias Kunnen <matthias.kunnen@gmail.com>
2023-05-12 22:52:47 +0200

    Ensure Timer link is active for all TimerComponent routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import {BreakpointObserver} from '@angular/cdk/layout';
 import type {OnDestroy, OnInit} from '@angular/core';
 import {Component, HostBinding} from '@angular/core';
 import {DateAdapter} from '@angular/material/core';
+import {NavigationEnd, NavigationSkipped, Router} from '@angular/router';
 import dateLocaleEn from 'date-fns/locale/en-GB';
 import {ReplaySubject} from 'rxjs';
 import {map, takeUntil} from 'rxjs/operators';
@@ -17,11 +18,14 @@ export class AppComponent implements OnDestroy, OnInit {
 
     @HostBinding('style.top') top: string;
 
+    isTimerPageActive = false;
+
     private readonly destroyed = new ReplaySubject<void>(1);
 
     constructor(
         private readonly dateAdapter: DateAdapter<any>,
         private readonly breakpointObserver: BreakpointObserver,
+        private readonly router: Router,
     ) {
     }
 
@@ -41,5 +45,13 @@ export class AppComponent implements OnDestroy, OnInit {
 
         this.dateAdapter.setLocale(dateLocaleEn);
         initErrorMessages();
+
+        this.router.events.pipe(
+            takeUntil(this.destroyed),
+        ).subscribe(event => {
+            if (event instanceof NavigationEnd || event instanceof NavigationSkipped) {
+                this.isTimerPageActive = event.url === '/' || event.url.startsWith('/join/');
+            }
+        });
     }
 }
