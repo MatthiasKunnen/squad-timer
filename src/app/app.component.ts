52bfe104b5dc840951b9c427a10f1624d1ed29b7
Matthias Kunnen <matthias.kunnen@gmail.com>
2023-05-13 01:00:07 +0200

    Use flex instead of position fixed for footer and header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
-import {BreakpointObserver} from '@angular/cdk/layout';
 import type {OnDestroy, OnInit} from '@angular/core';
-import {Component, HostBinding} from '@angular/core';
+import {Component} from '@angular/core';
 import {DateAdapter} from '@angular/material/core';
 import {NavigationEnd, NavigationSkipped, Router} from '@angular/router';
 import dateLocaleEn from 'date-fns/locale/en-GB';
 import {ReplaySubject} from 'rxjs';
-import {map, takeUntil} from 'rxjs/operators';
+import {takeUntil} from 'rxjs/operators';
 
 import {initErrorMessages} from './error/messages/message-init';
 
@@ -16,15 +15,12 @@ import {initErrorMessages} from './error/messages/message-init';
 })
 export class AppComponent implements OnDestroy, OnInit {
 
-    @HostBinding('style.top') top: string;
-
     isTimerPageActive = false;
 
     private readonly destroyed = new ReplaySubject<void>(1);
 
     constructor(
         private readonly dateAdapter: DateAdapter<any>,
-        private readonly breakpointObserver: BreakpointObserver,
         private readonly router: Router,
     ) {
     }
@@ -35,14 +31,6 @@ export class AppComponent implements OnDestroy, OnInit {
     }
 
     ngOnInit(): void {
-        this.breakpointObserver.observe('(max-width: 599px)').pipe(
-            map(value => value.matches ? 56 : 64),
-            map(value => `${value}px`),
-            takeUntil(this.destroyed),
-        ).subscribe(top => {
-            this.top = top;
-        });
-
         this.dateAdapter.setLocale(dateLocaleEn);
         initErrorMessages();
 
